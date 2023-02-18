59381131e66928087cd12df4416dffc98f9174c3
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-02-18 12:30:20 -0600

    Update profile module

--- src/store/index.js
+++ src/store/index.js
@@ -1,48 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import profile from './modules/profile'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    nameStore: '',
-    lastname: '',
-    email :''
-  },
-  getters: {
-    getName:state => {
-      return state.nameStore
-    },
-    getLastname:state => {
-      return state.lastname
-    },
-    getEmail:state => {
-      return state.email
-    },
-  },
-  mutations: {
-     setName :(state,othername) =>{
-      state.nameStore= othername;
-     },
-     setLastname :(state,othername) =>{
-      state.lastname= othername;
-     },
-     setEmail :(state,othername) =>{
-      state.email= othername;
-     }
-  },
-  actions: {
-  //ACTIONS ARE USED FOR API CALLS
-  //IN THIS CASE WE SIMULATE A SERVER CALLS WITH SETTIME OUT
-      actionProfile : (context,profile) => {
-            setTimeout(()=>{
-                context.commit('setName',profile.name);
-                context.commit('setLastname',profile.lastname);
-                context.commit('setEmail',profile.email);
-            },2000);
-      }
-  },
-  modules: {
-
+  modules:{
+    profile
   }
 })
