811ce4121315d9ce7523c009f565b70ff1761106
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-02-18 11:32:03 -0600

    Added new mapGetters ProfileDetailComponent in computed

--- src/components/ProfileDetailComponent.vue
+++ src/components/ProfileDetailComponent.vue
@@ -3,8 +3,7 @@
     <h4>Il mio Profilo</h4>
     <ul class="list-group list-group-flush">
       <li class="list-group-item"><b>Nome : </b>{{ getName }}</li>
-      <li class="list-group-item"><b>getNameSettings : </b>{{ getNameSettings }}</li>
-      <li class="list-group-item"><b>Cognome : </b>{{ getCognome }}</li>
+      <li class="list-group-item"><b>Cognome : </b>{{ getLastname }}</li>
       <li class="list-group-item"><b>Email : </b>{{ getEmail }}</li>
     </ul>
   </div>
@@ -27,7 +26,9 @@ export default {
   },
   computed : {
     ...mapGetters([
-          'getName'
+          'getName',
+          'getEmail',
+          'getLastname'
     ])
   },
   methods : {
