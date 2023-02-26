b5231c56d5f8ca38f35f82f0220dae48f019ad0f
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-02-25 19:32:22 -0600

    Added profile route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProfileDetailComponent from '../components/ProfileDetailComponent.vue'
+
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfileDetailComponent
   }
    /*,
   {
