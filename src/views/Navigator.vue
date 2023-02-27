8ce3cddba6b42872e86bdccf76509a6876835491
Valerio Pellegrino <pellegrino.valerio1@gmail.com>
2023-02-26 18:56:11 -0600

    Added Navigator component in views

---
+++ src/views/Navigator.vue
@@ -0,0 +1,29 @@
+<template>
+     <div id="nav">
+          <router-link to="/">Home</router-link> 
+          <router-link to="/profile">Profile</router-link> 
+          <router-link to="/profile-new">Profile</router-link> 
+     </div>
+
+</template>
+
+<script>
+export default {
+  name: 'navigatorView',
+  data (){
+    return {
+      
+    }
+  },
+  computed : {
+
+  },
+  methods : {
+  
+  },
+  components: {
+  //  'profile-detail-component' : ProfileDetailComponent,
+  //  'profile-component' : ProfileComponent 
+  }
+}
+</script>
